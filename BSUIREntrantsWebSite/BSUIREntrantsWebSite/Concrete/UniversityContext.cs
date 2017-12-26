161e7d395b4096b8c6a541a1538a838aacebf3d8
Lenovo-PC\Бороховская Ксения <kseniyaksu6a@gmail.com>
2017-12-27 01:01:46 +0300

    MyAwesomeCommit

---
+++ BSUIREntrantsWebSite/BSUIREntrantsWebSite/Concrete/UniversityContext.cs
@@ -0,0 +1,24 @@
+﻿using BSUIREntrantsWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace BSUIREntrantsWebSite.Concrete
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("EntrantsDataBase")
+        {
+        }
+
+        public DbSet<Enrollee> Entranst { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+        }
+    }
+}
---
+++ BSUIREntrantsWebSite/BSUIREntrantsWebSite/Models/Enrollee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSUIREntrantsWebSite.Models
+{
+    public class Enrollee
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string HomeTown { get; set; }
+        public int TotalScore { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
